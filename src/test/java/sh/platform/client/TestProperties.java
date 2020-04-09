0291c8fab8b03e2478eed696810c2940a0f521f3
otaviojava <otaviopolianasantana@gmail.com>
2020-04-09 12:53:51 -0300

    creates properties test

--- src/test/java/sh/platform/client/TestProperties.java
+++ src/test/java/sh/platform/client/TestProperties.java
@@ -1,4 +1,19 @@
 package sh.platform.client;
 
-public class PlatformshTestProperties {
+import java.util.function.Supplier;
+
+public enum  TestProperties implements Supplier<String> {
+
+    PROJECT("platform.api.project"), TOKEN("platform.api.token");
+
+    private final String value;
+
+    TestProperties(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
 }
